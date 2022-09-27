6a707bb905066176f67f14367ac91ae9d03624f6
Josh <zhaoxia2@ualberta.ca>
2022-09-27 16:13:52 -0600

    nothing

--- evaluation_mood.py
+++ evaluation_mood.py
@@ -318,6 +318,97 @@ def evaluation_DRAEM(args, model_denoise, model_segment, test_dataloader, epoch,
         
     return dice_value, auroc_px, auroc_sp
 
+def evaluation_reconstruction(args, model, test_dataloader, epoch, loss_function, run_name, threshold = 0.1):
+    
+    model.eval()
+    
+    dice_error = []
+        
+    gt_list_px = []
+    pr_list_px = []
+    gt_list_sp = []
+    pr_list_sp = []
+    aupro_list = []
+    pr_binary_list_px = []
+    
+    with torch.no_grad():
+        for img, gt, label, img_path, save in test_dataloader:
+
+            img = img.cuda()
+            gt[gt > 0.1] = 1
+            gt[gt <= 0.1] = 0
+            # check if img is RGB
+            rec = model(img)
+            
+            initial_feature = img
+
+            difference = cal_distance_map(rec[0,0,:,:].to('cpu').detach().numpy(), img[0,0,:,:].to('cpu').detach().numpy())            
+            # names = ['liver_1_60', 'liver_2_452', 'liver_3_394', 'liver_4_457', 'liver_6_396', 'liver_7_487', 'liver_10_379'] 
+            names = ['liver_1_59', 'liver_1_60', 'liver_1_66', 'liver_1_67', 'liver_2_415', 'liver_2_452', 'liver_3_394', 'liver_4_566', 'liver_4_457', 'liver_6_396', 'liver_8_448', 'liver_7_487', 'liver_10_328', 
+                            'liver_10_335' 'liver_10_356', 'liver_10_379',  'liver_10_295', 'liver_10_422' , 'liver_16_374', 'liver_16_409', 'liver_18_413', 'liver_19_440', 'liver_20_487', 'liver_21_388', 'liver_23_335',
+                            'liver_26_317', 'liver_27_414', 'liver_27_559', 'liver_13_393', 'liver_12_412', 'liver_11_413']
+            gt = gt[0,0,:,:].to('cpu').detach().numpy()  
+            
+            for name in names:
+                if name in img_path[0]:
+                    save_dir = os.path.join('/home/zhaoxiang', 'output', run_name, name)
+                    
+                    
+                    a_map_path = os.path.join(save_dir, 'a_map_{}.png'.format(epoch))
+                    p_map_path = os.path.join(save_dir, 'p_map_{}.png'.format(epoch))
+                    d_map_path = os.path.join(save_dir, 'd_map_{}.png'.format(epoch))
+                    initial_feature_path = os.path.join(save_dir, 'raw.png')
+                    reconstruction_path = os.path.join(save_dir, 'reconstruction_{}.png'.format(epoch))
+                    gt_path = os.path.join(save_dir, 'gt.png')
+                    anomaly_map_rgb_path = os.path.join(save_dir, 'combine.png')
+                    
+                    if not os.path.exists(save_dir):
+                        pathlib.Path(save_dir).mkdir(parents=True, exist_ok=True)
+                        
+                    # save inital_feature and reconstruction
+                    initial_feature = min_max_norm(initial_feature[0,:,:,:].to('cpu').detach().numpy())
+                    initial_feature = np.transpose(initial_feature, (1,2,0))
+                    cv2.imwrite(initial_feature_path, initial_feature*255)
+                    
+                    rec = min_max_norm(rec[0,:,:,:].to('cpu').detach().numpy())
+                    rec = np.transpose(rec, (1,2,0))
+                    cv2.imwrite(reconstruction_path, rec*255)
+                    
+                    cv2.imwrite(gt_path, gt * 255)
+                    # cv2.imwrite(d_map_path, difference*255)
+                                        
+                    # combine
+                    # label = cv2.imread(gt_path, cv2.IMREAD_GRAYSCALE)
+                    # anomaly_map_rgb = cv2.imread(a_map_path)
+                    # label = cv2.resize(label, [256, 256])                    
+                    # anomaly_map_rgb[:,:,1] = label
+                    # cv2.imwrite(anomaly_map_rgb_path, anomaly_map_rgb)
+                            
+                            
+                    prediction_map = np.where(difference > threshold, 255, 0)
+                    cv2.imwrite(a_map_path, difference*255)
+                    cv2.imwrite(p_map_path, prediction_map)
+            
+
+            # anomaly_map = gaussian_filter(anomaly_map, sigma=4)
+            # prediction_map = np.where(min_max_norm(anomaly_map) > threshold, 1, 0)
+            prediction_map = np.where(difference > threshold, 1, 0)
+
+        
+            gt_list_px.extend(gt.astype(int).ravel())
+            pr_list_px.extend(difference.ravel())
+            pr_binary_list_px.extend(prediction_map.ravel())
+            gt_list_sp.append(np.max(gt.astype(int)))
+            pr_list_sp.append(np.max(difference))
+        
+        # dice_value = mean(dice_error)
+        dice_value = dice(np.array(gt_list_px), np.array(pr_binary_list_px))
+        auroc_px = round(roc_auc_score(gt_list_px, pr_list_px), 3)
+        auroc_sp = round(roc_auc_score(gt_list_sp, pr_list_sp), 3)
+        
+        
+    return dice_value, auroc_px, auroc_sp
+
 def compute_pro(masks: ndarray, amaps: ndarray, num_th: int = 200) -> None:
     
     
