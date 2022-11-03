60e5d75853d035b9eb9506d6355aff941251f0c8
Josh <zhaoxia2@ualberta.ca>
2022-11-03 01:29:32 -0600

    threshold adjusted for the new dataset

--- train_reconstruction.py
+++ train_reconstruction.py
@@ -266,10 +266,10 @@ if __name__=="__main__":
     parser.add_argument("-img_size", "--img_size", type=float, default=256, help="noise magnitude.")
     
     # need to be changed/checked every time
-    parser.add_argument('--bs', default = 32, action='store', type=int)
+    parser.add_argument('--bs', default = 8, action='store', type=int)
     parser.add_argument('--gpu_id', default=['0','1'], action='store', type=str, required=False)
     parser.add_argument('--experiment_name', default='ColorJitter_reconstruction', choices=['DRAEM_Denoising_reconstruction, RandomShape_reconstruction, brain, head'], action='store')
-    parser.add_argument('--colorRange', default=100, action='store')
+    parser.add_argument('--colorRange', default=30, action='store')
     parser.add_argument('--threshold', default=200, action='store')
     parser.add_argument('--dataset_name', default='initial_hue', choices=['hist_DIY', 'Brain_MRI', 'CovidX', 'RESC_average'], action='store')
     parser.add_argument('--model', default='ws_skip_connection', choices=['ws_skip_connection', 'DRAEM_reconstruction', 'DRAEM_discriminitive'], action='store')
