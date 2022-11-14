acfc52348d18d6760b3a524ec6b3f8faaa1441a6
Josh <zhaoxia2@ualberta.ca>
2022-11-14 01:31:06 -0700

    cross fold validation update

--- dataloader_zzx.py
+++ dataloader_zzx.py
@@ -283,6 +283,155 @@ class MVTecDataset(torch.utils.data.Dataset):
                 
             save = False
             return img, gt, label, img_path, save
+        
+        
+class MVTecDataset_cross_validation(torch.utils.data.Dataset):
+    def __init__(self, root, transform, gt_transform, phase, dirs, data_source = 'liver', rgb=False, args=None):
+        if len(dirs) == 3:
+            [train_dir, test_dir, label_dir] = dirs
+        elif len(dirs) == 2:
+            [train_dir, test_dir] = dirs
+            
+        self.phase = phase
+        self.transform = transform
+        self.args = args
+        
+        self.cutpaste = CutPasteUnion(transform=self.transform)
+
+        self.gt_transform = gt_transform
+        self.data_source = data_source
+        self.rgb = rgb
+        
+        if phase == 'train':
+            self.img_path = os.path.join(root, 'train/good')
+            # self.img_paths = glob.glob(self.img_path + "/*.png")
+            if data_source == 'retina':
+                self.img_paths = glob.glob(self.img_path + "/*.bmp")
+            else:
+                self.img_paths = glob.glob(self.img_path + "/*.png")
+            self.img_paths.sort()
+        elif phase == 'test':
+            self.img_path = os.path.join(root, test_dir)
+            self.gt_path = os.path.join(root, label_dir)
+            # load dataset
+            self.img_paths, self.gt_paths = self.load_dataset()  # self.labels => good : 0, anomaly : 1
+            
+        elif phase == 'eval':
+            self.img_path = os.path.join(root, 'evaluation')
+            self.gt_path = os.path.join(root, 'evaluation_label')
+            self.img_paths, self.gt_paths = self.load_dataset()  # self.labels => good : 0, anomaly : 1
+
+    def load_dataset(self):         # only used in test phase
+        
+        img_tot_paths = []
+        gt_tot_paths = []
+        tot_labels = []
+        tot_types = []
+        
+        if self.data_source == 'retina':
+            defect_types = os.listdir(self.img_path)
+            for defect_type in defect_types:
+                if defect_type == 'good':
+                    img_paths = glob.glob(os.path.join(self.img_path, defect_type) + "/*.png")
+                    img_paths.sort()
+                    img_tot_paths.extend(img_paths)
+                    gt_tot_paths.extend(["/home/zhaoxiang/dataset/RESC_average/test_label/1.png"] * len(img_paths))
+
+                    tot_labels.extend([0] * len(img_paths))
+                    tot_types.extend(['good'] * len(img_paths))
+
+
+                elif defect_type == 'Ungood':
+                    img_paths = glob.glob(os.path.join(self.img_path, defect_type) + "/*.png")
+                    gt_paths = glob.glob(os.path.join(self.gt_path, defect_type) + "/*.png")
+                    img_paths.sort()
+                    gt_paths.sort()
+                    img_tot_paths.extend(img_paths)
+                    gt_tot_paths.extend(gt_paths)
+
+                    tot_labels.extend([1] * len(img_paths))
+                    tot_types.extend([defect_type] * len(img_paths))
+        
+        
+        else:
+            img_paths = glob.glob(self.img_path + "/*.png")
+            gt_paths = glob.glob(self.gt_path + "/*.png")            # ground truth mask.
+            img_paths.sort()                        # list them with a specific sequence
+            gt_paths.sort()
+            img_tot_paths.extend(img_paths)         # what does extend means? add all the elements of an iterrable to the end of the list.
+            gt_tot_paths.extend(gt_paths)
+
+            assert len(img_tot_paths) == len(gt_tot_paths), "Number of test and ground truth pair doesn't match!"          # if not, then raise an error.
+
+        return img_tot_paths, gt_tot_paths
+
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        
+        if self.phase == 'train':
+            img_path = self.img_paths[idx]
+            img = Image.open(img_path)
+            img = ImageOps.grayscale(img)
+            img = img.resize([self.args.img_size, self.args.img_size])
+            
+            """ Sample level augmentation"""
+            img_numpy = np.array(img)
+            if img_numpy.max() == 0:
+                img_tensor = self.transform(img)
+                img_tensor = img_tensor.repeat(2, 1, 1, 1)
+                gt_tensor = torch.zeros_like(img_tensor)
+                return img_tensor, img_tensor
+        
+            
+            colorJitter_img, colorJitter_gt = colorJitterRandom(img_numpy, colorRange=self.args.colorRange, threshold=self.args.threshold)
+            
+            colorJitter_img = np.expand_dims(colorJitter_img, axis=2)
+            
+            colorJitter_img = self.transform(colorJitter_img)
+            
+            """ Pixel level augmentation"""
+            org, cut_img, cut_gt = self.cutpaste(img)
+            # img = self.transform(img)
+            
+            img_list = [colorJitter_img]
+            gt_list = [colorJitter_gt]
+            gt_list = [(np.where(x > 0, 255, 0)).astype(np.uint8) for x in gt_list]
+            
+            gt_list = [self.transform(x) for x in gt_list]
+            gt_list = [x.unsqueeze(dim=0) for x in gt_list]
+            gt_tensor = torch.cat(gt_list, dim=0)
+            
+            img_list = [x.unsqueeze(dim=0) for x in img_list]
+            aug_tensor = torch.cat(img_list, dim = 0)
+            
+            org_tensor = (torch.unsqueeze(org, dim=0)).repeat(len(img_list), 1, 1, 1)
+            return org_tensor, aug_tensor, gt_tensor
+        
+        else:
+            img_path, gt_path= self.img_paths[idx], self.gt_paths[idx]
+            # img = Image.open(img_path).convert('RGB')s
+            img = Image.open(img_path)
+            img = ImageOps.grayscale(img)
+            img = img.resize([self.args.img_size, self.args.img_size])
+            if self.rgb:    
+                img = img.convert('RGB')
+            img = self.transform(img)
+            
+            gt = Image.open(gt_path)
+            gt = ImageOps.grayscale(gt)
+            gt = gt.resize([self.args.img_size, self.args.img_size])
+            gt = self.gt_transform(gt)
+
+            # determine the label
+            if torch.sum(gt) != 0:
+                label = 1
+            else:
+                label = 0
+                
+            save = False
+            return img, gt, label, img_path, save
     
     
     
